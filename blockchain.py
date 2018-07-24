e8555cf3171142d0904dee2bafaaf3f4fc3fc079
shubham jain <shubhamjain2908@gmail.com>
2018-07-25 00:58:19 +0530

    Verifying our Blockchain

--- blockchain.py
+++ blockchain.py
@@ -7,7 +7,7 @@ def get_last_blockchain_value():
         return None
     return blockchain[-1]
 
-def add_transaction(transaction_amount, last_transaction):
+def add_transaction(transaction_amount, last_transaction=[1]):
     """ Append a new value as well as the last blockchain value to the blockchain.
     
     Arguments:
@@ -32,10 +32,27 @@ def print_blockchain_elements():
         print('Outputting Block')
         print(block)
 
+def verify_chain():
+    """ Function to verify the blockchain """
+    block_index = 0
+    is_valid = True
+    for block in blockchain:
+        if block_index == 0:
+            block_index += 1
+            continue
+        elif block[0] == blockchain[block_index - 1]:
+            is_valid = True
+        else:
+            is_valid = False
+            break
+        block_index += 1
+    return is_valid        
+
 while True:
     print('Please choose')
     print('1: Add a new transaction')
     print('2: Output the blockchain blocks')
+    print('h: Manipulate the chain ')
     print('q: Quit')
     user_choice = get_user_choice()
     if user_choice == '1':
@@ -43,11 +60,16 @@ while True:
         add_transaction(tx_amount, get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain_elements()
+    elif user_choice == 'h':
+        if len(blockchain) >= 1:
+            blockchain[0] = [2]
     elif user_choice == 'q':
         break
     else:
         print('Input was invalid, please pick a value from the list! ')
-    print('Choice registered!')
+    if not verify_chain():
+        print('Invalid blockchain!')
+        break
         
     
 print('Done')
